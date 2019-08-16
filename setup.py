35adfbc80a24fe1df3c5554ef4e243f69abbbd1f
Peter Kosztolanyi <peter.kosztolanyi@transferwise.com>
2019-08-16 16:12:45 +0100

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='pipelinewise-tap-kafka',
-      version='1.0.0',
+      version='1.0.1',
       description='Singer.io tap for extracting data from Kafka topic - PipelineWise compatible',
       author='Stitch',
       url='https://singer.io',
