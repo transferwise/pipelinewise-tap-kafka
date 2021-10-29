2501966faf7a2afd8ecbeb8b3a875cd0a7ac7498
Peter Kosztolanyi <peter.kosztolanyi@transferwise.com>
2021-10-29 15:58:50 +0200

    Simplify custom exceptions

--- tap_kafka/errors.py
+++ tap_kafka/errors.py
@@ -2,13 +2,11 @@ class InvalidBookmarkException(Exception):
     """
     Exception to raise when bookmark is not valid
     """
-    def __init__(self, *args, **kwargs):
-        super().__init__(self, *args, **kwargs)
+    pass
 
 
 class DiscoveryException(Exception):
     """
     Exception to raise when discovery failed
     """
-    def __init__(self, *args, **kwargs):
-        super().__init__(self, *args, **kwargs)
+    pass
