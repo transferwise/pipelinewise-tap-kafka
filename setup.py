057c9d97f4316352629acafaefc6b9a52f7804ec
Peter Kosztolanyi <peter.kosztolanyi@transferwise.com>
2022-01-27 12:04:42 +0000

    Remove filelock requirement

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ setup(name='pipelinewise-tap-kafka',
       install_requires=[
           'pipelinewise-singer-python==2.*',
           'dpath==2.0.1',
-          'filelock==3.0.12',
           'confluent-kafka==1.7.0'
       ],
       extras_require={
